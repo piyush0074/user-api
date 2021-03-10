6aa8715140940178241bfeafbd2514558cf8b986
piyush0074 <piyushmakwana74@gmail.com>
2021-03-10 10:37:57 +0530

    It include funcitons of checkuser_id, check_user_email, password check and convert the password into hash(function name: hashpass) and get_user function for GET request.

---
+++ run/service.py
@@ -0,0 +1,50 @@
+from .models import CustomUser
+import crypt
+
+def checkuser_id(request):
+    try:
+        id = request.query_params["id"]
+        result = CustomUser.objects.get(id=id,is_deleted=False)
+        return result
+    except:
+        result = None
+        return result
+
+def check_user_email(user_name,user_email):
+    flag = ''
+    try:
+        result = CustomUser.objects.get(username=user_name,is_deleted=False)
+        if result.username == user_name :
+            flag = 'u'
+        if result.email == user_email :
+            flag = flag+'e'
+        return flag
+    except:
+        return flag
+
+def password_check(password):
+    num = False
+    upper = False
+    lower = False
+    if (len(password) <=5 or len(password) >17 ): return False
+
+    for char in range (0,len(password)):
+        if ( num  and upper and lower ): return True
+        if (not(num) and password[char].isdigit()): 
+            num = True
+            continue
+        if (not(lower) and password[char].islower()):
+            lower = True
+            continue
+        if (not(upper) and password[char].isupper()):
+            upper = True
+            continue
+    return False
+
+def hashpass(plaintext):
+    hash = crypt.crypt(plaintext)
+    return hash
+
+def get_user(request):
+    result = CustomUser.objects.get(username=request)
+    return result
